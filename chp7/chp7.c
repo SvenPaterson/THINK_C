bc12b800c0e63bb0af04d5d8030d557641ef170a
SvenPaterson <stephenjgarden@icloud.com>
2022-08-26 14:36:37 -0400

    rand() func added

--- chp7/chp7.c
+++ chp7/chp7.c
@@ -4,7 +4,12 @@
 double RandomFloat(int lower_bound, int upper_bound)
 {
     int x = rand();
-    return lower_bound + x % (upper_bound-1) + (double) x / RAND_MAX;
+    int term_two = x % (upper_bound-lower_bound);
+    double term_three = (double) x / RAND_MAX;
+    /* printf("\nlower bound: %i", lower_bound);
+    printf("\nx%%(upper_bound-lower_bound): %i\n", term_two);
+    printf("x / RAND_MAX: %.3f\n", term_three) */;
+    return lower_bound + term_two + term_three;
 
 }
 
@@ -14,11 +19,15 @@ int main(void)
     int i, length;
     length = sizeof(c) / sizeof(c[0]);
 
-    for (int i = 0; i < length; i++)
+    /* for (int i = 0; i < length; i++)
     {
         printf("%i\n", c[i]);
+    } */
+    
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%.3f\n", RandomFloat(100, 200));
     }
-
-    printf("%.3f", RandomFloat(100, 200));
+    
     return EXIT_SUCCESS;
 }

98609def2a564e63464d1f6d3bbb88a01d2d89e8
SvenPaterson <74683453+SvenPaterson@users.noreply.github.com>
2022-08-27 10:16:20 -0400

    Ex7.3 a) completed

--- chp7/chp7.c
+++ chp7/chp7.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 double RandomFloat(int lower_bound, int upper_bound)
 {
@@ -17,6 +18,7 @@ void RandomizeArray(int array[], int length, int lowerBound, int upperBound)
 {
     /* Will pass randomized values to array parameter, inclusive of the lower
     bound and exclusive of the upper bound */
+    srand(time(NULL));
     for(int i = 0; i < length; i++)
     {
         array[i] = lowerBound + rand() % (upperBound-lowerBound);
@@ -63,21 +65,61 @@ void Histogram(int array[], int array_len, int min, int max, int histogram[])
     }
 }
 
+int Search(int array[], int length, int target) 
+{
+    for(int i = 0; i < length; i++)
+    {
+        if (array[i] == target)
+        {
+            return i+1;
+        }
+        
+    }
+    return -1;
+}
+
+int IndexOfMaxInRange(int array[], int array_length)
+{
+    int big_num_pos = 0;
+    for(int i = 0; i < array_length-1; i++)
+    {
+        if (array[i] > array[big_num_pos])
+        {
+            printf("Number in position %i is bigger than %i\n", i, big_num_pos);
+            big_num_pos = i;
+        }
+    
+    }
+    return big_num_pos;
+}
+
 int main(void)
 {
 
-    int r_array_length = 20; // choose array size
+    int r_array_length = 100; // choose array size
     int lowerBound = 4; // choose low random number, lowerBound is inclusive
-    int upperBound = 10; // choose high random number, upperBound is exclusive
+    int upperBound = 100; // choose high random number, upperBound is exclusive
 
     int r_array[r_array_length];
     RandomizeArray(r_array, r_array_length, lowerBound, upperBound);
     PrintArray(r_array, r_array_length);
+    printf("\n");
 
+    /*
     int hist_len = upperBound - lowerBound;
     int histogram[hist_len];
     Histogram(r_array, r_array_length, lowerBound, upperBound, histogram);
-    PrintArray(histogram, hist_len);
+    PrintArray(histogram, hist_len); */
+
+    /* int number = 89;
+    int lastDigit = number%10;
+    int firstDigit = number/10;
+    printf("%i", lastDigit*10+firstDigit); */
+
+
+    int big_num_pos = IndexOfMaxInRange(r_array, r_array_length);
+    printf("Big Number Index: %i\n", big_num_pos);
+    printf("Big Number is %i", r_array[big_num_pos]);
 
     return EXIT_SUCCESS;
 }

cc10f91fa85560f0d631b625254f195728a856f1
SvenPaterson <stephenjgarden@icloud.com>
2022-09-02 13:06:00 -0400

    chp7 complete

--- chp7/chp7.c
+++ chp7/chp7.c
@@ -32,6 +32,7 @@ void PrintArray(int array[], int length)
     {
         printf("%i ", array[i]);
     }
+    printf("\n");
 }
 
 int HowMany(int array[], int length, int value)
@@ -49,9 +50,11 @@ int HowMany(int array[], int length, int value)
 
 void Histogram(int array[], int array_len, int min, int max, int histogram[])
 {
-    /* takes an array and the range of values in the array as 
+    /* 
+    takes an array and the range of values in the array as 
     two parameters min and max and stores a histogram of values
-    in a second array */
+    in a second array 
+    */
     int hist_len = max - min;
     for(int i = 0; i < hist_len; i++) 
     {
@@ -67,6 +70,10 @@ void Histogram(int array[], int array_len, int min, int max, int histogram[])
 
 int Search(int array[], int length, int target) 
 {
+    /*
+    Searches an array for a specific value and returns
+    its index, if not found returns -1
+    */
     for(int i = 0; i < length; i++)
     {
         if (array[i] == target)
@@ -78,32 +85,59 @@ int Search(int array[], int length, int target)
     return -1;
 }
 
-int IndexOfMaxInRange(int array[], int array_length)
+int IndexOfMaxInRange(int array[], int start_index, int stop_index)
 {
-    int big_num_pos = 0;
-    for(int i = 0; i < array_length-1; i++)
+    /*
+    Finds the largest value in a range between two given
+    indices and returns the index position of that value
+    */
+    int big_num_index = start_index;
+    for(int i = start_index; i <= stop_index; i++)
     {
-        if (array[i] > array[big_num_pos])
+        if (array[i] >= array[big_num_index])
         {
-            printf("Number in position %i is bigger than %i\n", i, big_num_pos);
-            big_num_pos = i;
+            big_num_index = i;
         }
-    
     }
-    return big_num_pos;
+    return big_num_index;
 }
+
+void SwapElement(int array[], int index_one, int index_two)
+{
+    /* 
+    Takes an array and two indices and swaps
+    the values found at each array index
+    */
+    int value_one = array[index_one];
+    int value_two = array[index_two];
+    array[index_one] = value_two;
+    array[index_two] = value_one;
+}
+
+void SortArray(int array[], int array_length)
+{
+    /*
+    Sorts an array from largest to smallest 
+    */
+    for (int i = 0; i < array_length; i++)
+    {
+        SwapElement(array, i, IndexOfMaxInRange(array, i, array_length));
+    }
+}
+
 
 int main(void)
 {
 
-    int r_array_length = 100; // choose array size
+    int r_array_length = 10; // choose array size
     int lowerBound = 4; // choose low random number, lowerBound is inclusive
-    int upperBound = 100; // choose high random number, upperBound is exclusive
+    int upperBound = 1000; // choose high random number, upperBound is exclusive
 
     int r_array[r_array_length];
     RandomizeArray(r_array, r_array_length, lowerBound, upperBound);
     PrintArray(r_array, r_array_length);
-    printf("\n");
+    SortArray(r_array, r_array_length);
+    PrintArray(r_array, r_array_length);
 
     /*
     int hist_len = upperBound - lowerBound;
@@ -116,10 +150,9 @@ int main(void)
     int firstDigit = number/10;
     printf("%i", lastDigit*10+firstDigit); */
 
-
-    int big_num_pos = IndexOfMaxInRange(r_array, r_array_length);
+    /* int big_num_pos = IndexOfMaxInRange(r_array, 0, r_array_length);
     printf("Big Number Index: %i\n", big_num_pos);
-    printf("Big Number is %i", r_array[big_num_pos]);
+    printf("Big Number is %i", r_array[big_num_pos]); */
 
     return EXIT_SUCCESS;
 }
